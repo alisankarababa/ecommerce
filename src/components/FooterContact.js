2d5193f54dbe3b3db69252610465487150fe795f
Alişan <alisankarababa@gmail.com>
2023-11-21 15:29:07 +0300

    add Footer
    add FooterColumn
    add FooterContact

---
+++ src/components/FooterContact.js
@@ -0,0 +1,13 @@
+export default function FooterContact() {
+
+    return (
+        <div>
+            <h5 className="text-clr-dark font-bold mb-[1.25rem]">Get In Touch</h5>
+            <div className="w-full">
+                <input className="px-[1.25rem] py-[0.9375rem]" type="text" placeholder="Your Email"/>
+                <input className="px-[1.40625rem] py-[0.9375rem] text-clr-light bg-bgclr-primary" type="submit" value="Subscribe"/>
+            </div>
+            <p className="text-clr-second">Lore imp sum dolor Amit</p>
+        </div>
+    );
+}
