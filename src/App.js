c1b2b77b34e0aa68b484ef778dd003ca68fa0e22
Alişan <alisankarababa@gmail.com>
2023-11-21 13:17:14 +0300

    add Header and CategoryCard ito App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+
+
+import men from "./assets/category-card/men.jpeg"
+import women from "./assets/category-card/women.jpeg"
+import accessory from "./assets/category-card/accessories.jpeg"
+import kids from "./assets/category-card/kids.jpeg"
+
+
+import CategoryCard from './components/CategoryCard';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App font-fnt-mont">
+        <Header />
+
+        <section >
+            <div className='w-[80%] flex gap-x-[2rem] aspect-[2/1]'>
+            <div className='grow-[2] basis-0'>
+                <CategoryCard className="w-full h-full" urlImg={men} textBtn={"MEN"}/>
+            </div>
+            <div className='grow-[1] basis-0'>
+                <CategoryCard className="w-full h-full" urlImg={women} textBtn={"WOMEN"}/>
+            </div>
+            <div className='grow-[1] basis-0 flex flex-col gap-y-[1rem]'>
+                <CategoryCard className="grow basis-0" urlImg={accessory} textBtn={"WOMEN"}/>
+                <CategoryCard className="grow basis-0" urlImg={kids} textBtn={"KIDS"}/>
+            </div>
+            </div>
+        </section>
     </div>
   );
 }
