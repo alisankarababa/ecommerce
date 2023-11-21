8c56ea54974fe9a084edbd558c0da649fb5d0f70
Alişan <alisankarababa@gmail.com>
2023-11-21 14:13:05 +0300

    add class img-absolute
    for images to be restricted inside a div without expanding it

--- src/components/CategoryCard.js
+++ src/components/CategoryCard.js
@@ -4,7 +4,7 @@ export default function CategoryCard({urlImg, textBtn, className}) {
 
     return (
         <div className={className}>
-            <img className="h-full w-full object-cover absolute translate-x-1/2 right-1/2" src={urlImg} alt="img-category"/>
+            <img className="img-absolute" src={urlImg} alt="img-category"/>
             <button className="bg-bgclr-light text-clr-dark font-bold min-w-[33.33%] py-[0.75em] px-[1.625em] absolute left-[1rem] bottom-[1rem] z-10">{textBtn}</button>
         </div>
     );
