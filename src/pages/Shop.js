09e8ac6fc6417e71aedeba687afd1765f570a967
Alişan <alisankarababa@gmail.com>
2023-11-23 20:11:22 +0300

    add product cards to shop page

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -1,5 +1,8 @@
 import Clients from "../components/Clients";
+import ProductCard from "../components/ProductCard";
 import ShopCard from "../components/ShopCard";
+
+import product from "../assets/product/example-product.jpeg"
 import imgShopCard1 from "../assets/Shop/shopCard1.jpeg";
 
 export default function Shop() {
@@ -27,8 +30,46 @@ export default function Shop() {
 				    </section>
                 </div>
 
+                <div>
+				    <section className="container-big px-[12.25em]">    
+				    	<div className="text-clr-second flex items-center justify-between py-[1.5em]">
+				    		<div className="text-[0.875rem] font-bold">
+				    			Showing all 12 results
+				    		</div>
+				    		<div className="flex items-center gap-x-[1rem]">
+				    			<span className="text-[0.875rem] font-bold">Views:</span>
+				    			<i className="p-[1rem] rounded-[5px] border-[1px] border-clr-light-gray-2 text-clr-dark fa-solid fa-border-all"></i>
+				    			<i className="p-[1rem] rounded-[5px] border-[1px] border-clr-light-gray-2 text-clr-dark fa-solid fa-list-ul"></i>
+				    		</div>
+				    		<div className="flex">
+				    			<select
+				    				className="mr-[1rem] pr-[2em] pl-[1.3em] text-[0.875rem] border-[1px] rounded-[5px] border-[#DDDDDD] bg-[#F9F9F9]"
+				    				name="cars"
+				    				id="cars"
+				    			>
+				    				<option value="Popularity">Popularity</option>
+				    			</select>
+				    			<button className="font-bold btn-small btn-primary text-[0.875rem]">
+				    				Filter
+				    			</button>
+				    		</div>
+				    	</div>
+				    	<div className="my-[3rem] flex flex-wrap gap-y-[5rem] gap-x-[1.875rem]">
+				    		{Array(8)
+				    			.fill(1)
+				    			.map((item, idx) => {
+				    				return (
+				    					<ProductCard
+				    						key={idx}
+				    						className="basis-[239px]"
+				    						urlImg={product}
+				    					/>
+				    				);
+				    			})}
+				    	</div>
 				    </section>
                 </div>
 				<Clients />
 			</>
+		);
 }
