c1bda1e6e8fd599b3ba330bb4f01b83fdecc84c4
Alişan <alisankarababa@gmail.com>
2023-11-27 15:25:49 +0300

    add Path components
    -use it in Shop pages
    -update PageContent so that it routes /shop/product to Product pages

--- src/layout/PageContent.js
+++ src/layout/PageContent.js
@@ -2,11 +2,15 @@ import { Switch, Route, Redirect } from 'react-router-dom/cjs/react-router-dom';
 
 import Shop from "../pages/Shop"
 import Home from "../pages/Home"
+import Product from "../pages/Product"
 
 export default function PageContent() {
 	return (
         
 		<Switch>
+            <Route path="/shop/product">
+                <Product />
+			</Route>
 			<Route path="/shop">
 				<Shop />
 			</Route>
