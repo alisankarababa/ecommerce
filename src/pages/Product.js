e6038a7db0d9e15dc8000dc11b61f56c5b41ed95
Alişan <alisankarababa@gmail.com>
2023-11-28 15:42:48 +0300

    add Path display  to Product page

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -6,6 +6,11 @@ import SectionProductBestSeller from "../components/product-page/SectionProductB
 export default function Product() {
 	return (
 		<>
+			<div className="bg-bgclr-ligth-gray-1">
+				<div className="container-big px-[1rem] py-[1.5rem]">
+					<Path />
+				</div>
+			</div>
 
             <ProductNavBar />
 			<SectionMid />
