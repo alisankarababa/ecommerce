a3d49b7591815c9f4ea9a2756fceeaf813256a19
Alişan <alisankarababa@gmail.com>
2023-11-29 22:10:48 +0300

    add SectionText to about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,12 +2,14 @@
 import Clients from "../components/Clients"
 import Statistics from "../components/product-page/Statistics"
 import SectionTeam from "../components/product-page/SectionTeam";
+import SectionText from "../components/product-page/SectionText";
 
 export default function About() {
     return (
 			<>
-                <SectionTeam />
+                <SectionText />
                 <Statistics />
+                <SectionTeam />
 				<Clients />
 			</>
 		);
