7dbb47be229ff0a963522a06ce4f67feaea87aaf
Alişan <alisankarababa@gmail.com>
2023-11-30 14:23:02 +0300

    refactor SectionTeam
    -move it to components so taht it can be used in Team page
    -add component TeamDisplay

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,7 @@
 
 import Clients from "../components/Clients"
 import Statistics from "../components/about-page/Statistics"
-import SectionTeam from "../components/about-page/SectionTeam";
+import SectionTeam from "../components/SectionTeam";
 import SectionText from "../components/about-page/SectionText";
 import SectionImgVideo from "../components/about-page/SectionImgVideo";
 import HeroAbout2 from "../components/about-page/HeroAbout2";
