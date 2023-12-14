460e7207b9a08b4209bdb9b5fc3a0e4329953fe7
Alişan <alisankarababa@gmail.com>
2023-12-14 23:47:39 +0300

    add responsiveness to SectionHero in About page

--- src/pages/About.js
+++ src/pages/About.js
@@ -11,15 +11,19 @@ export default function About() {
     return (
 			<>
                 <div className="bg-bgclr-light">
-                    <div className="max-w-[1157px] m-auto">
-                        <section className="flex flex-wrap items-center min-h-[625px] gap-[2.5625rem]">
-                            <div className="basis-[380px] grow-[5] mb-[2rem] font-bold flex flex-col items-left gap-y-[2.1875rem] text-left">
-                                <h5 className="text-clr-dark">ABOUT COMPANY</h5>
-                                <h1 className="text-clr-dark text-[3.625rem]">ABOUT US</h1>
-                                <p className="text-clr-second text-[1.25rem] max-w-[380px]">We know how large objects will act, but things on a small scale</p>
-                                <button className="btn-primary btn-small-wide self-start">Get Quote Now</button>
+                    <div className="max-w-[1100px] m-auto px-[1rem]">
+                        <section className="grid grid-cols-autofill-minmax20rem1fr auto-rows-[40rem]">
+                            <div className="flex items-center justify-start">
+                                <div className="font-bold flex flex-col gap-y-[2.1875rem] md:text-left md:w-full">
+                                    <h5 className="text-clr-dark">ABOUT COMPANY</h5>
+                                    <h1 className="text-clr-dark text-[2.5rem] sm:text-[3.625rem]">ABOUT US</h1>
+                                    <p className="text-clr-second text-[1.25rem] max-w-[380px] m-auto md:m-0">We know how large objects will act, but things on a small scale</p>
+                                    <div>
+                                        <button className="btn-primary btn-small-wide">Get Quote Now</button>
+                                    </div>
+                                </div>
                             </div>
-                            <div className="basis-[460px] grow-[2] self-stretch">
+                            <div className="">
                                 <img className="w-full h-full object-cover" src={imgHero} alt="img"/>
                             </div>
                         </section>
