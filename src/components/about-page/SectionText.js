e82612af59c75fc958e1e5018f3547af8a798ce0
Alişan <alisankarababa@gmail.com>
2023-12-14 23:23:08 +0300

    add responsiveness to SectionText in about page

--- src/components/about-page/SectionText.js
+++ src/components/about-page/SectionText.js
@@ -2,8 +2,8 @@ export default function SectionText() {
     return (
         <div className="bg-bgclr-light">
             <div className="container-small">
-                <section className="text-left py-[1.5em] flex flex-wrap items-center justify-between gap-[3.75rem]">
-                <div className="basis-[24.625rem]">
+                <section className="py-[1.5em] lg:text-left flex flex-wrap items-center justify-center gap-[3.75rem] max-w-[70%] m-auto lg:max-w-full">
+                <div className="basis-[24.625rem] ">
                     <p className="text-clr-danger text-[0.875rem] mb-[1.5rem]">Problems trying</p>
                     <p className=" text-clr-dark font-bold text-[1.5rem]">Met minim Mollie non desert Alamo est sit cliquey dolor do met sent.</p>
                 </div>
