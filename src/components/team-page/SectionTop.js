5612e98a9f123c21248dd4f0ee4765bfc6273162
Alişan <alisankarababa@gmail.com>
2023-12-14 17:14:15 +0300

    add responsiveness to SectionTop Team Page

--- src/components/team-page/SectionTop.js
+++ src/components/team-page/SectionTop.js
@@ -4,9 +4,9 @@ export default function SectionTop() {
 	return (
 		<div className="bg-bgclr-light">
 			<div className="container-small">
-				<div className="w-[77%] m-auto py-[3.125em]">
+				<div className="py-[3.125em]">
 					<h1 className="text-clr-second font-bold mb-[1rem]">WHAT WE DO</h1>
-					<p className="text-clr-dark font-bold text-[3.625rem] mb-[1rem]">
+					<p className="mb-[1rem] text-clr-dark font-bold text-[2.5rem] sm:text-[3.625rem]">
 						Innovation tailored for you
 					</p>
 					<Path className="justify-center" />
