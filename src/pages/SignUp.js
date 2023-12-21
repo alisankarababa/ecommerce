7fdc938ba2b165eedf85ec5e3909a3687b75997f
Alişan <alisankarababa@gmail.com>
2023-12-21 18:14:27 +0300

    refactor form using InputRHF

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -17,6 +17,10 @@ function InputRHF({labelText, inputType, errors, inputKey, inputRegistration}) {
         </div>
     );
 }
+
+
+function SingUpForm({roles}) {
+    
     const { register, handleSubmit, watch, formState: { errors, isValid } } = useForm({mode: "onBlur"});
     const onSubmit = data => console.log(data);
 
@@ -26,108 +30,101 @@ function InputRHF({labelText, inputType, errors, inputKey, inputRegistration}) {
             <div className='max-w-[400px] m-auto px-[1rem] py-[5rem] sm:py-[10rem]'>
                 <form className="m-auto" onSubmit={handleSubmit(onSubmit)}>
                     <div >
-                        <div className='input-group'>
-                            <label className="input-label" htmlFor="name">Name</label>
-                            <input
-                                className="input-field"
-                                id='name'
-                                type="text"
-                                {...register("name",
-                                    {
-                                        required: {
-                                            value: true,
-                                            message: "Name is required."
-                                        },
-                                        minLength: {
-                                            value: 3,
-                                            message: "Name should be at least 3 characters long."
-                                        },
-                                        maxLength: {
-                                            value: 30,
-                                            message: "Name cannot be longer than 30 characters."
-                                        }
+                        
+                        <InputRHF
+                            labelText="Name"
+                            inputType="text"
+                            inputKey="name"
+                            errors={errors}
+                            inputRegistration= {
+                                register("name",
+                                {
+                                    required: {
+                                        value: true,
+                                        message: "Name is required."
+                                    },
+                                    minLength: {
+                                        value: 3,
+                                        message: "Name should be at least 3 characters long."
+                                    },
+                                    maxLength: {
+                                        value: 30,
+                                        message: "Name cannot be longer than 30 characters."
                                     }
-                                )}
-                            />
-                            {errors.name && <p role="alert" className='input-error'>{errors.name?.message}</p>}
-                        </div>
-            
-                        <div className='input-group'>
-                            <label className="input-label" htmlFor="email">Email</label>
-                            <input
-                                className="input-field"
-                                id='email'
-                                type="email"
-                                {...register("email",
-                                    {
-                                        required: {
-                                            value: true,
-                                            message: "Email is required."
-                                        },
-                                        minLength: {
-                                            value: 15,
-                                            message: "Email must be at least 15 characters long."
-                                        },
-                                        maxLength: {
-                                            value: 100,
-                                            message: "Email cannot be longer than 100 characters."
-                                        },
-                                        pattern: {
-                                            value: /^(([^<>()[\]\.,;:\s@"]+(\.[^<>()[\]\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-                                            message: "Invalid email"
-                                        }
+                                })
+                            }
+                        />
+                        
+                        <InputRHF 
+                            labelText="Email"
+                            inputKey="email"
+                            inputType="email"
+                            errors={errors}
+                            inputRegistration={
+                                register("email",
+                                {
+                                    required: {
+                                        value: true,
+                                        message: "Email is required."
+                                    },
+                                    minLength: {
+                                        value: 15,
+                                        message: "Email must be at least 15 characters long."
+                                    },
+                                    maxLength: {
+                                        value: 100,
+                                        message: "Email cannot be longer than 100 characters."
+                                    },
+                                    pattern: {
+                                        value: /^(([^<>()[\]\.,;:\s@"]+(\.[^<>()[\]\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                                        message: "Invalid email"
                                     }
-                                )}
-                            />
-                            {errors.email && <p className='input-error'>{errors.email.message}</p>}
-                        </div>
+                                })
+                            }
+                        />
+                            
+                        <InputRHF
+                            labelText="Password"
+                            inputKey="password"
+                            inputType="password"
+                            errors={errors}
+                            inputRegistration={
+                                register("password",
+                                {
+                                    required: {
+                                        value: true,
+                                        message: "Password is required."
+                                    },
+                                    minLength: {
+                                        value: 8,
+                                        message: "Password must be at least 8 characters long."
+                                    },
+                                    maxLength: {
+                                        value: 30,
+                                        message: "Password cannot be longer than 30 characters."
+                                    },
+                                    pattern: {
+                                        value: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+{}\[\]:;<>,.?~\-]).+$/,
+                                        message: "Password must contain at least one number, one lower case letter, one upper case letter and a special character."
+                                    }
+                                })
+                            }
+                        />
 
-                        <div className='input-group'>
-                            <label className="input-label" htmlFor='password'>Password</label>
-                            <input
-                                className="input-field"
-                                id='password'
-                                type="password"
-                                {...register("password",
-                                    {
-                                        required: {
-                                            value: true,
-                                            message: "Password is required."
-                                        },
-                                        minLength: {
-                                            value: 8,
-                                            message: "Password must be at least 8 characters long."
-                                        },
-                                        maxLength: {
-                                            value: 30,
-                                            message: "Password cannot be longer than 30 characters."
-                                        },
-                                        pattern: {
-                                            value: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+{}\[\]:;<>,.?~\-]).+$/,
-                                            message: "Password must contain at least one number, one lower case letter, one upper case letter and a special character."
-                                        }
+                        <InputRHF
+                            labelText="Confirm Password"
+                            inputKey="confirm_password"
+                            inputType="password"
+                            errors={errors}
+                            inputRegistration={
+                                register("confirm_password",
+                                {
+                                    validate: (fieldValue) => {
+                                        return watch("password") === fieldValue || "Password does not match.";
                                     }
-                                )}
-                            />
-                            {errors.password && <p className='input-error'>{errors.password.message}</p>} 
-                        </div>
-
-                        <div className='input-group'>
-                            <label className="input-label" htmlFor='confirm-password'>Confirm Password</label>
-                            <input
-                                className="input-field"
-                                id='confirm-password'
-                                type="password"
-                                {...register("confirm_password",
-                                    {
-                                        validate: (fieldValue) => {
-                                            return watch("password") === fieldValue || "Password does not match.";
-                                        }
-                                    }
-                                )}
-                            />
-                            {errors.confirm_password && <p className='input-error'>{errors.confirm_password.message}</p>} 
-                        </div>
+                                })
+                            }
+                        />
 
                         <div className='input-group'>
                             <label className="input-label" htmlFor='role'>Role</label>
