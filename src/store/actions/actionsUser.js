59dae5db7534bf7fb68c669c7fec2ba091a9ec00
Alişan <alisankarababa@gmail.com>
2023-12-29 00:07:19 +0300

    add reducerUser and actions for it

---
+++ src/store/actions/actionsUser.js
@@ -0,0 +1,4 @@
+export const eActionsUser = Object.freeze({
+
+
+});
--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -5,4 +5,5 @@ import reducerGlobal from './reducerGlobal';
 
 export const reducers = combineReducers({
     reducerGlobal,
+    reducerUser,
 })
