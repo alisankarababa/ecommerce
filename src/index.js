8976fcea677bd4404c90ff021aed1ef33ed951f5
Alişan <alisankarababa@gmail.com>
2023-12-28 19:37:50 +0300

    remove strict mode

--- src/index.js
+++ src/index.js
@@ -12,7 +12,6 @@ import { ToastContainer } from 'react-toastify';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <BrowserRouter>
         <ToastContainer
             position="top-right"
@@ -28,7 +27,6 @@ root.render(
         />
         <App />
     </BrowserRouter>
-  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
