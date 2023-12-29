a8a61ba43cee2ce38bc7c57d1120720ac837dfdd
Alişan <alisankarababa@gmail.com>
2023-12-29 11:04:55 +0300

    add Reducer user to combineReders function

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 
 import reducerGlobal from './reducerGlobal';
+import reducerUser from './reducerUser'
 import reducerShoppingCart from './reducerShoppingCart';
 
 export const reducers = combineReducers({
     reducerGlobal,
+    reducerUser,
     reducerShoppingCart
 })
