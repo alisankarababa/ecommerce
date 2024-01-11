e367526dc1899cb2636080e3e657571268bd5f50
Alişan <alisankarababa@gmail.com>
2024-01-11 16:04:16 +0300

    add token setter

--- src/store/reducers/reducerUser.js
+++ src/store/reducers/reducerUser.js
@@ -3,6 +3,7 @@ import {eActionsUser} from "../actions/actionsUser"
 
 const initialState = {
     loggedInUser: null,
+    token: null,
     errorLoggingIn: null,
     isLoggingInInProgress: false,
 }
@@ -14,7 +15,7 @@ export default function reducerUser(state=initialState, action) {
             return { ...state, errorLoggingIn: null, isLoggingInInProgress: true};
 
         case eActionsUser.LOGIN_REQUEST_SUCCESSFUL:
-            return { ...state, loggedInUser: action.payload };
+            return { ...state, loggedInUser: action.payload, token: action.payload.token };
 
         case eActionsUser.LOGIN_REQUEST_FAILED:
             return { ...state, errorLoggingIn: action.payload };
