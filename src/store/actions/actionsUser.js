6d016dfa4080c2187ed18069d817aa0029dc564b
Alişan <alisankarababa@gmail.com>
2024-01-11 14:12:53 +0300

    add login related procedures
    -to actionsUser and
    -to reducerUser

--- src/store/actions/actionsUser.js
+++ src/store/actions/actionsUser.js
@@ -1,4 +1,45 @@
+import { api } from "../../api/api";
+
 export const eActionsUser = Object.freeze({
-
-
+    LOGIN_SENDING_REQUEST: "actionsUser/ login request in progress",
+    LOGIN_REQUEST_SUCCESSFUL: "actionsUser/ logged in successfully",
+    LOGIN_REQUEST_FAILED: "actionsUser/ login failed",
+    LOGIN_REQUEST_ENDED: "actionsUser/ login request finalized",
 });
+
+export function  actionCreatorUserLogin(loginRequestBody, isLoginSuccessFullCallBack) {
+    
+    return function funcDispatch(dispatch) {
+        
+        dispatch(actionCreatorLogin());
+
+        api.post("/login", loginRequestBody)
+            .then(response => {
+                dispatch(actionCreatorLoginSuccess(response.data));
+                isLoginSuccessFullCallBack(true);
+            })
+            .catch(error => {
+                dispatch(actionCreatorLoginFailed(error.message));
+                isLoginSuccessFullCallBack(false);
+            })
+            .finally(() => {
+                dispatch(actionCreatorLoginEnded());
+            })
+    }
+}
+
+function actionCreatorLogin() {
+    return { type: eActionsUser.LOGIN_SENDING_REQUEST };
+}
+
+function actionCreatorLoginSuccess(data) {
+    return {type: eActionsUser.LOGIN_REQUEST_SUCCESSFUL, payload: data}
+}
+
+function actionCreatorLoginFailed(error) {
+    return {type: eActionsUser.LOGIN_REQUEST_FAILED, payload: error}
+}
+
+function actionCreatorLoginEnded() {
+    return { type: eActionsUser.LOGIN_REQUEST_ENDED }
+}
--- src/store/reducers/reducerUser.js
+++ src/store/reducers/reducerUser.js
@@ -2,14 +2,26 @@ import {eActionsUser} from "../actions/actionsUser"
 
 
 const initialState = {
-
+    loggedInUser: null,
+    errorLoggingIn: null,
+    isLoggingInInProgress: false,
 }
 
 export default function reducerUser(state=initialState, action) {
 
     switch (action.type) {
+        case eActionsUser.LOGIN_SENDING_REQUEST:
+            return { ...state, errorLoggingIn: null, isLoggingInInProgress: true};
 
-        
+        case eActionsUser.LOGIN_REQUEST_SUCCESSFUL:
+            return { ...state, loggedInUser: action.payload };
+
+        case eActionsUser.LOGIN_REQUEST_FAILED:
+            return { ...state, errorLoggingIn: action.payload };
+
+        case eActionsUser.LOGIN_REQUEST_ENDED:
+            return { ...state, isLoggingInInProgress: false };
+
         default:
             return state;
     }
