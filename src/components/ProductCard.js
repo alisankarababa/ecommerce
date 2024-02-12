ff282f1bd5110d637389fbb26ece99127a87d771
Alişan <alisankarababa@gmail.com>
2024-02-12 12:32:55 +0300

    truncate product desc

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -9,9 +9,9 @@ export default function ProductCard({ productName, price, rating, description, u
 			<div className="relative w-full aspect-[21/26] sm:aspect-[56/100]">
 				<img className="img-absolute" src={urlImg} alt="img-product" />
 			</div>
-			<div className="flex flex-col grow gap-y-[0.625rem]">
+			<div className="flex flex-col gap-y-[0.625rem]">
 				<h5 className="text-clr-dark font-bold">{productName}</h5>
-				<span className="text-[0.875rem] text-clr-second">{description}</span>
+				<span className="text-[0.875rem] text-clr-second max-w-[200px] whitespace-nowrap overflow-hidden text-ellipsis">{description}</span>
                 <span className="text-clr-secondary-1">{price} ₺</span>
                 <span className="text-clr-secondary-1">puan: {rating}</span>
 				<div className="flex justify-center gap-x-[0.3125rem]">
