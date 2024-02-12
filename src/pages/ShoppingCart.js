62b43c13b1e822e3d9262f5de4456d598a189d33
Alişan <alisankarababa@gmail.com>
2024-02-12 16:01:42 +0300

    add order summary box

--- src/pages/ShoppingCart.js
+++ src/pages/ShoppingCart.js
@@ -13,6 +13,8 @@ import {
 	actionCreatorShoppingCartToggleCheck,
 } from "../store/actions/actionsShoppingCart";
 
+import { useEffect, useState } from "react";
+
 function CountButtonGroup({
 	currentCount,
 	hIncrement,
@@ -150,6 +152,58 @@ function CartTable() {
 		</TableContainer>
 	);
 }
+
+function OrderSummaryBox() {
+
+	const cart = useSelector((store) => store.reducerShoppingCart.cart);
+    const [ productCost, setProductCost ] = useState(0);
+    const [ freeDelivery, setFreeDelivery ] = useState(false);
+    const [ totalCost, setTotalCost ] = useState(0);
+    
+
+    const freeDeliveryLimit = 150;
+    const shippingCost = 29.99;
+
+
+    useEffect(() => {
+
+        const localTotalPrice = cart.reduce( ( acc, cartItem ) => {
+
+            if(cartItem.checked)
+                return acc + cartItem.product.price * cartItem.count;
+
+            return acc;
+        } , 0)
+
+        setProductCost(localTotalPrice);
+
+        const localIsFreeDelivery = localTotalPrice > freeDeliveryLimit ? true : false;
+
+        setFreeDelivery( localIsFreeDelivery );
+
+        setTotalCost( localIsFreeDelivery ? localTotalPrice : localTotalPrice + shippingCost );
+
+    }, [cart, setProductCost])
+
+
+    return (
+        <div className="p-[1rem] border-[1px] rounded-[5px] border-clr-second text-left text-clr-dark">
+            <h5 className="mb-[1rem] font-medium">Sipariş Özeti</h5>
+            <div className="grid grid-cols-2 gap-[1.125rem] text-[0.875rem]">
+                <span>Ürün Toplamı</span>
+                <span className="text-right font-semibold">{productCost.toFixed(2)} TL</span>
+                <span>Kargo Toplamı</span>
+                <span className="text-right font-semibold">{shippingCost} TL</span>
+                <span>{freeDeliveryLimit} TL ve Üzeri Kargo Bedava (Satıcı Karşılar)</span>
+                <span className="text-right font-semibold">{freeDelivery ? -shippingCost.toFixed(2) : 0.00} TL</span>
+            </div>
+            <hr className="border-t-[1px] border-clr-second my-[1rem]"/>
+            <div className="grid grid-cols-2 gap-[1.125rem] text-[0.875rem]">
+                <span>Toplam</span>
+                <span className="text-right font-semibold text-clr-primary">{totalCost.toFixed(2)} TL</span>
+            </div>
+        </div>
+    );
 }
 
 export default function ShoppingCart() {
@@ -161,7 +215,14 @@ export default function ShoppingCart() {
 			<p className="text-left mb-[1.25rem] font-semibold">
 				Sepetim ({cart.length} Ürün)
 			</p>
-            <CartTable />
+			<div className="flex">
+				<div className="flex-[4]">
+					<CartTable />
+				</div>
+				<div className="flex-[1]">
+					<OrderSummaryBox />
+				</div>
+			</div>
 		</div>
 	);
 }
