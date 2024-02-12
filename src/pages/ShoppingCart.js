820a14f7adaa17280ee50175f0d22d69bf82440f
Alişan <alisankarababa@gmail.com>
2024-02-12 15:55:55 +0300

    modify style of table rows

--- src/pages/ShoppingCart.js
+++ src/pages/ShoppingCart.js
@@ -55,75 +55,93 @@ function CartTable() {
 						<TableRow
 							key={cartItem.product.id}
 							className="h-[150px] p-[1rem]"
-							sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+							sx={{
+								"&:last-child td, &:last-child th": { border: 0 },
+								display: "flex",
+							}}
 						>
-							<TableCell className="w-[90px]" component="th" scope="row">
-								<input
-									type="checkbox"
-									name="myCheckbox"
-									checked={cartItem.checked}
-									onChange={() =>
-										dispatch(
-											actionCreatorShoppingCartToggleCheck(cartItem.product.id)
-										)
-									}
-								/>
+							<TableCell component="th" scope="row" sx={{ flex: "0.2" }}>
+								<div className="h-full flex justify-center items-center">
+									<input
+										type="checkbox"
+										name="myCheckbox"
+										checked={cartItem.checked}
+										onChange={() =>
+											dispatch(
+												actionCreatorShoppingCartToggleCheck(
+													cartItem.product.id
+												)
+											)
+										}
+									/>
+								</div>
 							</TableCell>
-							<TableCell className="w-[90px]" component="th" scope="row">
-								<img
-									className="w-full h-full object-cover"
-									src={cartItem.product.images[0].url}
-									alt="product_img"
-								/>
+							<TableCell sx={{ flex: "1" }} component="th" scope="row">
+								<div className="m-auto h-full aspect-[2/3]">
+									<img
+										className="w-full h-full object-cover"
+										src={cartItem.product.images[0].url}
+										alt="product_img"
+									/>
+								</div>
 							</TableCell>
 							<TableCell
-								className="max-w-[400px]"
-								sx={{ display: { sm: "none", md: "table-cell" } }}
+								sx={{ display: { sm: "none", md: "table-cell" }, flex: "5" }}
 								component="th"
 								scope="row"
 							>
-								<span className="text-clr-dark font-semibold">
-									{cartItem.product.name}
-								</span>{" "}
-								<span>{cartItem.product.description}</span>
+								<div className="h-full flex items-center">
+									<div>
+										<span className="text-clr-dark font-semibold">
+											{cartItem.product.name}
+										</span>{" "}
+										<span>{cartItem.product.description}</span>
+									</div>
+								</div>
 							</TableCell>
-							<TableCell align="right">
-								<span className="text-clr-primary font-semibold">
-									{(cartItem.product.price * cartItem.count).toFixed(2)} TL
-								</span>
+							<TableCell sx={{ flex: "1" }}>
+								<div className="h-full flex justify-end items-center">
+									<span className="text-clr-primary font-semibold">
+										{(cartItem.product.price * cartItem.count).toFixed(2)} TL
+									</span>
+								</div>
 							</TableCell>
-							<TableCell>
-								<CountButtonGroup
-									currentCount={cartItem.count}
-									hIncrement={() =>
-										dispatch(
-											actionCreatorShoppingCartIncrementProductCount(
-												cartItem.product.id
+							<TableCell sx={{ flex: "1" }}>
+								<div className="h-full flex justify-center items-center">
+									<CountButtonGroup
+										currentCount={cartItem.count}
+										hIncrement={() =>
+											dispatch(
+												actionCreatorShoppingCartIncrementProductCount(
+													cartItem.product.id
+												)
 											)
-										)
-									}
-									hDecrement={() =>
-										dispatch(
-											actionCreatorShoppingCartDecrementProductCount(
-												cartItem.product.id
+										}
+										hDecrement={() =>
+											dispatch(
+												actionCreatorShoppingCartDecrementProductCount(
+													cartItem.product.id
+												)
 											)
-										)
-									}
-									minCount={1}
-									maxCount={10}
-								/>
+										}
+										minCount={1}
+										maxCount={10}
+									/>
+								</div>
 							</TableCell>
-							<TableCell align="right">
-								<i
-									onClick={() =>
-										dispatch(
-											actionCreatorShoppingCartRemoveProduct(
-												cartItem.product.id
+							<TableCell align="center" sx={{ flex: "0.4" }}>
+								<div className="h-full flex justify-center items-center">
+									<i
+										onClick={() =>
+											dispatch(
+												actionCreatorShoppingCartRemoveProduct(
+													cartItem.product.id
+												)
 											)
-										)
-									}
-									className="fa-regular fa-trash-can hover:text-clr-danger text-[1.25rem]"
-								></i>
+										}
+										className="fa-regular fa-trash-can hover:text-clr-danger text-[1.25rem]"
+									></i>
+								</div>
 							</TableCell>
 						</TableRow>
 					))}
@@ -131,6 +149,7 @@ function CartTable() {
 			</Table>
 		</TableContainer>
 	);
+}
 }
 
 export default function ShoppingCart() {
