cf6f9c8e337e779c1326b7a68df10c6ed766f486
Alişan <alisankarababa@gmail.com>
2024-03-01 13:38:13 +0300

    refactor InputRHF

--- src/components/InputRHF.js
+++ src/components/InputRHF.js
@@ -1,21 +1,42 @@
-export default function InputRHF({labelText, inputType, errors, inputKey, inputRegistration}) {
+export default function InputRHF({labelText, inputType, errors, inputKey, inputRegistration, children}) {
 
     const errorMessage = getNestedValue(errors, inputKey)?.message;
 
     function getNestedValue(obj, key) {
         return key.split('.').reduce((acc, currentKey) => acc && acc[currentKey], obj);
     }
+    
+    const renderInput = () => {
+        switch (inputType) {
+            case 'text':
+            case 'email':
+            case 'password':
+            case 'number':
+            case 'date':
+                return <input className="input-field" id={inputKey} type={inputType} {...inputRegistration} />;
+            case 'select':
+                return (
+                    <select className="input-field" id={inputKey} {...inputRegistration}>
+                        {children}
+                    </select>
+                );
+            case 'radio':
+                return (
+                    <input className="input-field" id={inputKey} type={inputType} {...inputRegistration} />
+                );
+
+            default:
+                return <input className="input-field" id={inputKey} type="text" {...inputRegistration} />;
+        }
+    };
 
     return (
         <div className='input-group'>
-
-            <label className="input-label" htmlFor={inputKey}>{labelText}</label>
-            <input
-                className="input-field"
-                id={inputKey}
-                type={inputType}
-                {...inputRegistration}
-            />
+            {
+                labelText &&
+                <label className="input-label" htmlFor={inputKey}>{labelText}</label>
+            }
+            {renderInput()}
             {errorMessage && <p role="alert" className='input-error'>{errorMessage}</p>}
         </div>
     );
